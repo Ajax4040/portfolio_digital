71c567616fe175b78b96fce392e42101004656aa
Apx <apx.jul@gmail.com>
2024-01-19 19:25:16 -0300

    se agrega una pantalla de carga

--- js/index.js
+++ js/index.js
@@ -1,3 +1,51 @@
+//---------------------------------------------------------------------------
+//Animacion inicial para el launcher
+const rocket = document.getElementById('rocket');
+const dialog = document.getElementById('dialog-box');
+
+// Inicialmente, coloca tanto el rocket como el dialog-box fuera de vista
+rocket.style.transform = 'translateY(130%)';
+dialog.style.transform = 'translateY(350%)';
+
+//Header
+const header = document.querySelector('header');
+
+//Ocultar el div con id loadingScreende despues de 3 segundos de entrar a la pagina
+window.addEventListener('load', () => {
+    const preload = document.querySelector('#loadingScreen');
+    setTimeout(() => {
+        console.log('se oculto el div');
+        preload.style.display = 'none';
+    }, 1000);
+
+    // Espera un momento adicional antes de iniciar la animación del header
+    setTimeout(() => {
+        header.style.animation = 'slideDown 1.5s ease forwards';
+    }, 500);
+
+    // Aplica la animación inicial de entrada al cohete
+    setTimeout(() => {
+        rocket.style.transition = 'transform 6s ease';
+        rocket.style.transform = 'translateY(0)';
+    }, 1000);
+
+    setTimeout(() => {
+        rocket.removeAttribute('style');
+    }, 7000);
+
+    // Espera a que la animación del cohete termine para iniciar la del cuadro de diálogo
+    setTimeout(() => {
+        dialog.style.transition = 'transform 2s ease'; // Duración de transición más corta para dialog-box
+        dialog.style.transform = 'translateY(0)';
+        // Agrega la animación floating2 después de que la animación de entrada termine
+        setTimeout(() => {
+            dialog.classList.add('floating-animation');
+        }, 2000); // Ajusta este tiempo al tiempo de duración de la animación de entrada del dialog
+    }, 7000); // Ajusta este tiempo al tiempo de duración de la animación de entrada del rocket
+
+});
+
+
 //Animación de la sección de "About me" al hacer scroll---------------------
 const aboutMeSection = document.querySelector('#about-me');
 const aboutMeH3 = document.querySelector('#about-me-h3');
@@ -15,35 +63,6 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(aboutMeSection);
-
-//---------------------------------------------------------------------------
-//Animacion inicial para el launcher
-const rocket = document.getElementById('rocket');
-const dialog = document.getElementById('dialog-box');
-
-// Inicialmente, coloca tanto el rocket como el dialog-box fuera de vista
-rocket.style.transform = 'translateY(130%)';
-dialog.style.transform = 'translateY(350%)';
-
-// Aplica la animación inicial de entrada al cohete
-setTimeout(() => {
-    rocket.style.transition = 'transform 6s ease';
-    rocket.style.transform = 'translateY(0)';
-}, 1000);
-
-setTimeout(() => {
-    rocket.removeAttribute('style');
-}, 7000);
-
-// Espera a que la animación del cohete termine para iniciar la del cuadro de diálogo
-setTimeout(() => {
-    dialog.style.transition = 'transform 2s ease'; // Duración de transición más corta para dialog-box
-    dialog.style.transform = 'translateY(0)';
-    // Agrega la animación floating2 después de que la animación de entrada termine
-    setTimeout(() => {
-        dialog.classList.add('floating-animation');
-    }, 2000); // Ajusta este tiempo al tiempo de duración de la animación de entrada del dialog
-}, 7000); // Ajusta este tiempo al tiempo de duración de la animación de entrada del rocket
 
 //----------------------------------------------------------------------------
 //Animación para las imagenes de las redes en la seccion "contact"
